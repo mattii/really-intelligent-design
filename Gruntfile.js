6476a90b165394c8481cd5e894adfaa2d60f317a
Johannes Ewald <johannes.ewald@peerigon.com>
2014-12-22 12:18:05 +0100

    Add grunt tasks to concatenate and auto-reload compiled grid stylesheets

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            gss: {
+                files: ["styles/*.gss"],
+                tasks: ["css_import"],
+                options: {
+                    // Start another live reload server on port 1337
+                    livereload: true
+                }
+            }
+        },
+        css_import: {
+            "test/assets/page.gss": ["styles/page.gss"],
+            "test/assets/modal.gss": ["styles/modal.gss"],
+        },
+        connect: {
+            test: {
+                port: 3000
+            }
+        },
+        concurrent: {
+            watchAndServe: ["connect", "watch"]
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-css-import");
+    grunt.loadNpmTasks("grunt-connect");
+    grunt.loadNpmTasks("grunt-concurrent");
+
+    grunt.registerTask("default", ["css_import", "concurrent:watchAndServe"]);
+};
